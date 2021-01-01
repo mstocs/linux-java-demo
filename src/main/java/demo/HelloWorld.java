bf55da9857d5c5426a93f189524493db8f5420a4
Constant <constant@YuandeMacBook-Pro.local>
2021-01-01 21:28:19 +0800

    finish

---
+++ src/main/java/demo/HelloWorld.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("hello world!");
+        }
+    }
+}
